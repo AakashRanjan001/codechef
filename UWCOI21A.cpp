0d0b0fa1966ee7b6f42d50f0c4bd1672337b04ef
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2023-12-30 15:02:28 +0530

    Add files via upload

---
+++ UWCOI21A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    
+	    
+	    
+	    int N; 
+	    cin>>N;
+	   
+
+	    cout<<1<<" "<<N <<endl;
+
+	}
+
+}
