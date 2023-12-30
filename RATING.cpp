a2769867ae432335d0b9164f21ba8e2946aceb3c
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2023-12-30 14:37:06 +0530

    Add files via upload

---
+++ RATING.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x,y;
+	    int s;
+	    cin>>s;
+	    
+	    cout<<-(s+1)<<endl;
+	    
+	    
+	    
+	    
+	
+	}
+
+}
