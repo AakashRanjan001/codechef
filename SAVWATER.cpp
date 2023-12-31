74230a6b732910a91be409084ceeeff140a72533
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2023-12-31 14:50:31 +0530

    918

---
+++ SAVWATER.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int H,x,y,C;
+	for(int i=0;i<t;i++){
+		int H,x,y,C;
+		cin>>H>>x>>y>>C;
+	
+	if(H*(x + y/2)<=C){
+	    cout<<"yes"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+}
+}
