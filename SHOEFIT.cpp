9d43413b873fce9fe0757a0a902023084dcf9482
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2023-12-31 15:32:06 +0530

    Add files via upload

---
+++ SHOEFIT.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+
+for(int i=0;i<t;i++){
+    int A,B,C;
+    cin>>A>>B>>C;
+    if(A+B+C>0 && A+B+C<3){
+        cout<<1<<endl;
+    }  else{
+        cout<<0<<endl;
+    }
+
+    }
+ 
+
+
+}
