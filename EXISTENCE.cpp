0d05f40875bce8b14bc6febd600e3f603d68d486
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-01-01 12:22:20 +0530

    Add files via upload

---
+++ EXISTENCE.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	for(int i=0;i<t;i++){
+	    long long int x,y;
+	    cin>>x>>y;
+	    
+	  
+	     if(x*x==2*y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
