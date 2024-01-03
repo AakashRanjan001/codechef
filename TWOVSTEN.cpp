a0d9e4f2132cabe534e7c6b0a0facc97b0876077
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-01-03 13:37:13 +0530

    Add files via upload

---
+++ TWOVSTEN.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++){
+
+   int x;
+   cin>>x;
+   
+   if(x%10==0){
+       cout<<0<<endl;
+   }
+   else if(x%5==0 && x%10!=0){
+       cout<<1<<endl;
+   }
+   else{
+       cout<<-1<<endl;
+   }
+}
+}
