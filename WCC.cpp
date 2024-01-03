94d1c867a3030b2c191163b447aa72181998897b
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-01-03 12:17:59 +0530

    Add files via upload

---
+++ WCC.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main() {
+	int t, x, c, n, d;
+
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    string s;
+	    cin>>s;
+	    
+	    c = 0, n = 0, d = 0;
+	    for (int i = 0; i < 14; i++) {
+	        if(s[i] == 'C'){
+	            c++;
+	        }
+	        else if(s[i] == 'D'){
+	            d++;
+	        }
+	        else if(s[i] == 'N'){
+	            n++;
+	        }
+	    }
+	    
+	    if(c > n){
+	        cout<<60*x<<endl;
+	    }
+	    else if(c == n){
+	        cout<<55*x<<endl;
+	    }
+	    else{
+	        cout<<40*x<<endl;
+	    }
+	}
+	return 0;
+}
+
+
