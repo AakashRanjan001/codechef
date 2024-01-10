f2c62d5aac2121bb47daf8e19b247b09194f6761
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-01-10 18:52:44 +0530

    nice q

---
+++ TEKKEN.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	 
+	 int a,b,c;
+	 cin>>a>>b>>c;
+	    
+	    int x = b -min(b,c);
+	    int y = c -min(b,c);
+	    
+	    if(x>y){
+	        a = a-x;
+	        if(a>0){
+	            cout<<"YES"<<endl;
+	        }
+	        else{
+	            cout<<"NO"<<endl;
+	        }
+	    }
+	    else{
+	        a = a-y;
+	           if(a>0){
+	            cout<<"YES"<<endl;
+	        }
+	        else{
+	            cout<<"NO"<<endl;
+	        }
+	        
+	    }
+	    
+	    
+	    
+	    
+	    
+	}
+
+}
