6e2d0e720f18474eabc1ab1990768d2e3a2968ac
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-01-12 19:18:59 +0530

    Add files via upload

---
+++ DIGARR.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    
+	    int n;
+	    cin>>n;
+	    
+	    string str;
+	    cin>>str;
+	    
+	    int b=0;
+	    for(int i=0;i<n;i++){
+	        if(str[i]=='0'|| str[i]=='5'){
+	            b++;
+	        }
+	        
+	        
+	    }
+	    if(b>0){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
