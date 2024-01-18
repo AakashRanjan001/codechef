318a56a21b69712b9e6d45b3fba1b7cfecc67772
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-01-19 00:17:55 +0530

    Add files via upload

---
+++ ADASCOOL.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    
+	    if(n*m%2==0){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	    
+	}
+
+}
