b0a8402344ac5d946fdf057d65dd631b52f5fd2d
AakashRanjan001 <144724018+AakashRanjan001@users.noreply.github.com>
2024-01-20 01:21:29 +0530

    Add files via upload

---
+++ ZCOSCH.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	
+	if(x>=1 && x<=50){
+	    cout<<100<<endl;
+	}
+	else if(x>50 && x<=100){
+	    cout<<50<<endl;
+	}
+	else{
+	    cout<<0<<endl;
+	}
+
+}
